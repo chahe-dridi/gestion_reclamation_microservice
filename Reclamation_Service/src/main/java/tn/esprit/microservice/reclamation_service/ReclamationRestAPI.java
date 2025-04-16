47209861d61e84a739f2943eccc06924a3a0d0e7
chahe-dridi <chaher.dridi.100@gmail.com>
2025-04-16 16:22:16 +0100

    added qr code

--- Reclamation_Service/src/main/java/tn/esprit/microservice/reclamation_service/ReclamationRestAPI.java
+++ Reclamation_Service/src/main/java/tn/esprit/microservice/reclamation_service/ReclamationRestAPI.java
@@ -3,8 +3,17 @@ package tn.esprit.microservice.reclamation_service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.Map;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/reclamations")
@@ -12,8 +21,14 @@ public class ReclamationRestAPI {
 
     private final ReclamationService reclamationService;
 
-    public ReclamationRestAPI(ReclamationService reclamationService) {
+    private final QRCodeService qrCodeService;
+
+ 
+
+    public ReclamationRestAPI(ReclamationService reclamationService, QRCodeService qrCodeService, PdfService pdfService) {
         this.reclamationService = reclamationService;
+        this.qrCodeService = qrCodeService;
+     
     }
 
     @PostMapping
@@ -47,4 +62,36 @@ public class ReclamationRestAPI {
     public String sayHello() {
         return "Hello from Reclamation Microservice!";
     }
+
+
+    // Endpoint to generate QR code for a
+    // http://localhost:8083/reclamations/1/qr
+    @GetMapping("/{id}/qr")
+    public ResponseEntity<String> generateQRCode(@PathVariable int id) {
+        try {
+            String url = qrCodeService.generateQRCodeForReclamation(id);
+            return ResponseEntity.ok(url);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Erreur lors de la génération du QR Code: " + e.getMessage());
+        }
+    }
+    // Endpoint to serve QR code image
+    // http://localhost:8093/reclamations/qr/1.png
+    @GetMapping("/qr/{filename:.+}")
+    public ResponseEntity<ByteArrayResource> serveQrCodeImage(@PathVariable String filename) throws IOException {
+        Path filePath = Paths.get("src/main/resources/static/reclamation-qr/", filename);
+        if (!Files.exists(filePath)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        byte[] imageBytes = Files.readAllBytes(filePath);
+        ByteArrayResource resource = new ByteArrayResource(imageBytes);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=" + filename)
+                .contentType(MediaType.IMAGE_PNG)
+                .contentLength(imageBytes.length)
+                .body(resource);
+    }
+
 }
